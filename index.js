fc6aea98b45b93bffb10205eb83a494264f4df9a
tony <antolisah@gmail.com>
2021-10-04 18:49:25 +0300

    use chalk to show password and copy to clipbaord with clipboardy

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const program = require('commander');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+
 const createPassword = require('./utils/createPassword');
 const log = console.log;
 
@@ -18,5 +21,9 @@ const { length, save, numbers, symbols } = program.opts();
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols);
 
+// copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
 // Output generated password
-log(generatedPassword);
+log(chalk.blue('Generated password: ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Copied to clipboard'));
